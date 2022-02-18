e4b1b1a4b4ac69d6e02de2f1c16200201f373fc9
bessemdh <bbendhaou@anypli.com>
2022-02-18 22:17:51 +0100

    fix-migrations : add soft delete column

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,11 @@ class User extends Authenticatable
     use SoftDeletes,HasApiTokens, HasFactory, Notifiable;
 
     /**
+     * @var string
+     */
+    protected $dateFormat = 'U';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
